ae2590eadaef2ec8b2ade85a924387afd74913d3
Norik25 <n.nazarej@edu.salford.ac.uk>
2018-06-20 07:31:22 +0100

    change2006:07:30

--- ajax.php
+++ ajax.php
@@ -24,6 +24,7 @@ try {
         $productSize = $_POST['size'];
         $productPrice = $_POST['price'];
         $productQuantity = $_POST['quantity'];
+        $productImg='';
         $start = $_POST['start'];
         $limit = $_POST['limit'];
 
@@ -52,10 +53,26 @@ try {
             if (!empty($itemDataSet)) {
                 exit('Produkt s timto nazvem a farbou uz existuje');
             } else {
-                echo $productPrice;
+
+
+                if (isset($_FILES['attachments'])) {
+                    $imgExtention = rand(false);
+                    $msg = "";
+                    $productImg = $imgExtention . basename($_FILES['attachments']['name'][0]);
+                    $targetFile = "Uploads/" . $productImg;
+
+                    if (file_exists($targetFile))
+                        $msg = array("status" => 0, "msg" => "File already exists!");
+                    else if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $targetFile))
+                        $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => $targetFile);
+
+                    exit(json_encode($msg));
+                }
+
+
                 $addItemQuery = ("INSERT INTO NASSA_items (item_name, item_type, item_color, 
-              item_price, item_brand, item_size, item_quantity) VALUES (:productName, :productType, 
-               :productColor, :productPrice, :productBrand,:productSize, :productQuantity)");
+              item_price, item_brand, item_size, item_quantity, item_img) VALUES (:productName, :productType, 
+               :productColor, :productPrice, :productBrand,:productSize, :productQuantity, :productImg)");
                 $stmt = $dbConnection->prepare($addItemQuery);
                 $stmt->bindParam(':productName', $productName, PDO::PARAM_STR);
                 $stmt->bindParam(':productType', $productType, PDO::PARAM_STR);
@@ -64,6 +81,7 @@ try {
                 $stmt->bindParam(':productSize', $productSize, PDO::PARAM_STR);
                 $stmt->bindParam(':productPrice', $productPrice, PDO::PARAM_STR);
                 $stmt->bindParam(':productQuantity', $productQuantity, PDO::PARAM_INT);
+                $stmt->bindParam(':productImg', $productImg, PDO::PARAM_STR);
 
                 if ($stmt->execute()) {
                     exit('Produkt bol uspesne ulozeny v databaze.' );
